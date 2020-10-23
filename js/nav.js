360f9a62f5bb6f30808a35e4659e825399edd10d
Hugo lópez <hugoilias>
2020-10-22 21:28:55 -0500

    [ADD] index.html, css, js, imagenes y animate.css

--- js/nav.js
+++ js/nav.js
@@ -15,3 +15,19 @@ $(document).ready(function () {
   });
 });
 
+
+let animado = document.querySelectorAll('.cont-tec');
+
+const mostrarScroll = () => {
+
+  let scrollTop = document.documentElement.scrollTop;
+  for (let i = 0; i < animado.length; i++) {
+    let alturaAnimado = animado[i].offsetTop;
+    if (alturaAnimado - 300 < scrollTop) {
+      animado[i].style.opacity = 1;
+      animado[i].classList.add('mostrar');
+    }
+  }
+}
+
+window.addEventListener('scroll', mostrarScroll);
